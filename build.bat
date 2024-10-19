bb70a3e23e6c4d036a64d778b05c999d57111885
lsmmega <pogmegalsm@outlook.com>
2024-10-20 00:05:56 +0800

    create killed

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 bmp2nes gfx/obj/tank/e_tank.bmp
 bmp2nes gfx/obj/capsule/large_wpn_capsule.bmp
 bmp2nes gfx/obj/wpn/mega_buster.bmp
+bmp2nes gfx/obj/killed.bmp
 bmp2nes gfx/obj/bar.bmp
 asm6 main.asm rm2.nes
 @pause
